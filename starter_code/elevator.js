0206cb6321fcc02f36f8d384fbd35480b906304e
Alla Roman <alla.roman.mail@gmail.com>
2018-01-30 09:15:03 +0100

    Not finished

--- starter_code/elevator.js
+++ starter_code/elevator.js
@@ -1,19 +1,46 @@
 class Elevator {
-  constructor(){
-    this.floor      = 0;
-    this.MAXFLOOR   = 10;
-    this.requests   = [];
+  constructor () {
+    this.floor = 0;
+    this.MAXFLOOR = 10;
+    this.requests = [];
+    this.direction = ['Up', 'Down'];
   }
 
-  start() { }
-  stop() { }
-  update() { }
-  _passengersEnter() { }
-  _passengersLeave() { }
-  floorUp() { }
-  floorDown() { }
-  call() { }
-  log() { }
+  update () {
+    log();
+  }
+
+  log () {
+    console.log('Direction: ' + this.direction + 'Floor' + this.floor);
+  }
+
+  start () {
+    setInterval(() => {
+      update();
+    }, 1000);
+  }
+
+  stop () {
+    clearInterval();
+  }
+
+  // _passengersEnter () { }
+  // _passengersLeave () { }
+  floorUp () {
+    if (this.floor <= 10) {
+      this.floor++;
+    } else {
+      console.log('You are at the highest floor');
+    }
+  }
+  floorDown () {
+    if (this.floor > 0) {
+      this.floor--;
+    } else {
+      console.log('You are on the ground floor');
+    }
+  // call () { }
+  }
 }
 
 module.exports = Elevator;
